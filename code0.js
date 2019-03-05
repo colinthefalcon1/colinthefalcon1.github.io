a89c7b51ecac856ac97743a8ca20f7b1d63fe1b4
Colin M <colin.d.miller@icloud.com>
2019-03-05 14:53:29 -0600

    asdfghjkl

--- code0.js
+++ code0.js
@@ -1,4 +1,10 @@
 gdjs.melCode = {};
+gdjs.melCode.GDWazowskiObjects1= [];
+gdjs.melCode.GDWazowskiObjects2= [];
+gdjs.melCode.GDMelObjects1= [];
+gdjs.melCode.GDMelObjects2= [];
+gdjs.melCode.GDFloorObjects1= [];
+gdjs.melCode.GDFloorObjects2= [];
 
 gdjs.melCode.conditionTrue_0 = {val:false};
 gdjs.melCode.condition0IsTrue_0 = {val:false};
@@ -6,11 +12,28 @@ gdjs.melCode.condition0IsTrue_0 = {val:false};
 
 gdjs.melCode.eventsList0xb0cf8 = function(runtimeScene) {
 
+{
+
+
+{
+gdjs.melCode.GDWazowskiObjects1.createFrom(runtimeScene.getObjects("Wazowski"));
+{gdjs.evtTools.camera.centerCamera(runtimeScene, (gdjs.melCode.GDWazowskiObjects1.length !== 0 ? gdjs.melCode.GDWazowskiObjects1[0] : null), true, "", 0);
+}}
+
+}
+
+
 }; //End of gdjs.melCode.eventsList0xb0cf8
 
 
 gdjs.melCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
+gdjs.melCode.GDWazowskiObjects1.length = 0;
+gdjs.melCode.GDWazowskiObjects2.length = 0;
+gdjs.melCode.GDMelObjects1.length = 0;
+gdjs.melCode.GDMelObjects2.length = 0;
+gdjs.melCode.GDFloorObjects1.length = 0;
+gdjs.melCode.GDFloorObjects2.length = 0;
 
 gdjs.melCode.eventsList0xb0cf8(runtimeScene);
 return;

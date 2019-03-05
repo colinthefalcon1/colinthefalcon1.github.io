796a2cc8fcf49acbd6aa228d546531166402d753
Colin M <colin.d.miller@icloud.com>
2019-03-05 14:25:05 -0600

    gam

---
+++ code0.js
@@ -0,0 +1,18 @@
+gdjs.melCode = {};
+
+gdjs.melCode.conditionTrue_0 = {val:false};
+gdjs.melCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.melCode.eventsList0xb0cf8 = function(runtimeScene) {
+
+}; //End of gdjs.melCode.eventsList0xb0cf8
+
+
+gdjs.melCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.melCode.eventsList0xb0cf8(runtimeScene);
+return;
+}
+gdjs['melCode'] = gdjs.melCode;

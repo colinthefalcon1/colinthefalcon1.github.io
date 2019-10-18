933c26bcefff156ad2d092d5e4aeafe499f273cc
colinthefalcon1 <47410519+colinthefalcon1@users.noreply.github.com>
2019-10-18 13:03:48 -0500

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'db4free.net:3306/bigboymeatmarket');
-   define('DB_USERNAME', 'tedcruz');
-   define('DB_PASSWORD', 'cm!122203');
-   define('DB_DATABASE', 'bigboymeatmarket');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>

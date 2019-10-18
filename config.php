b4e2c2155e447cadfa7867f3a30067378fe47981
colinthefalcon1 <47410519+colinthefalcon1@users.noreply.github.com>
2019-10-18 14:03:01 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'remotemysql.com:3306');
+   define('DB_USERNAME', 'NlcwiUGnZ2');
+   define('DB_PASSWORD', 'YraCsOHyaJ');
+   define('DB_DATABASE', 'NlcwiUGnZ2');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>

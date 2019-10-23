b939faf527d659b3708b0e4452b7d62758a20f5d
colinthefalcon1 <47410519+colinthefalcon1@users.noreply.github.com>
2019-10-22 20:21:44 -0500

    Update loadingscreen-pixi-renderer.js

--- pixi-renderers/loadingscreen-pixi-renderer.js
+++ pixi-renderers/loadingscreen-pixi-renderer.js
@@ -11,14 +11,14 @@ gdjs.LoadingScreenPixiRenderer = function(runtimeGamePixiRenderer, loadingScreen
   this._loadingScreen.addChild(this._progressText);
 
   if (loadingScreenSetup && loadingScreenSetup.showGDevelopSplash) {
-    this._madeWithText = new PIXI.Text('Made with', {
+    this._madeWithText = new PIXI.Text('Big Boy Meat Market', {
       fontSize: '30px',
       fontFamily: 'Arial',
       fill: '#FFFFFF',
       align: 'center',
     });
     this._madeWithText.position.y = this._pixiRenderer.height / 2 - 130;
-    this._websiteText = new PIXI.Text('gdevelop-app.com', {
+    this._websiteText = new PIXI.Text('Big Boy Meat Market', {
       fontSize: '30px',
       fontFamily: 'Arial',
       fill: '#FFFFFF',

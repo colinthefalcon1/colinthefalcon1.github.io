4cb64f99fe51fa12996d933ce68ca1ce0d7d71b7
colinthefalcon1 <47410519+colinthefalcon1@users.noreply.github.com>
2019-12-16 10:20:46 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["username"]; ?><br>
-Your email address is: <?php echo $_POST["password"]; ?>
+Welcome <?php echo $_GET["username"]; ?><br>
+Your email address is: <?php echo $_GET["password"]; ?>
 
 </body>
 </html>
